803d482d4d205256fa9c4c9da8a4fe655e394880
AlessiaGo <alessiagolino@hotmail.it>
2023-08-28 16:07:39 +0200

    (Added) pages of the project

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello Wrlod')
+    return render(request, 'pages/index.html')
+
+def about(request):
+     return render(request, 'pages/about.html')
 
