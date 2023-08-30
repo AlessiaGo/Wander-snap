2759eb0ab498a4e37f6199c274f034fbeec80d18
AlessiaGo <alessiagolino@hotmail.it>
2023-08-30 11:03:32 +0200

    (Added) listings page and creators page

--- wander/settings.py
+++ wander/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'creators.apps.CreatorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
