cbb0bf736b6bf142e0a4c099ff944ad1aa951d98
AlessiaGo <alessiagolino@hotmail.it>
2023-08-31 18:06:35 +0200

    (Fixed) db, models and admin

--- creators/models.py
+++ creators/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Creator(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
+from creators.models import Creator
 
-# Create your models here.
+class Listing(models.Model):
+    creator = models.ForeignKey(Creator, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.title
